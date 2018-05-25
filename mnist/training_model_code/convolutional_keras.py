2c929cd299e0bcae482c690494ce4e628c18b752
anupamamurthi <anupama.murthi@gmail.com>
2018-05-25 11:53:23 -0400

    remove print statements

--- mnist/training_model_code/convolutional_keras.py
+++ mnist/training_model_code/convolutional_keras.py
@@ -75,15 +75,10 @@ model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
-
-# model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
-#           verbose=1, validation_data=(x_test, y_test))
-
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1)
 
 score = model.evaluate(x_test, y_test, verbose=0)
 
-print(score)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
